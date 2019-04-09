1b19a772cfad7d49d6514c0472e243f371f4174c
Chloe Strainge <nullptr.0@gmail.com>
2019-04-09 16:46:18 -0700

    Bad tabbing.

--- bin/vectrex/src/main.rs
+++ bin/vectrex/src/main.rs
@@ -9,14 +9,14 @@ use vectrex_core::{ engine::Engine, os::shell::WindowProps };
 fn main()
 {
     let mut engine = Engine::new(WindowProps
-            {
-                title: "New Game +",
-                full_screen: false,
-                resizable: true,
-                width: 800,
-                height: 600
-            })
-            .unwrap_or_else(|error_message| panic!("Error starting engine: {}", error_message));
+        {
+            title: "New Game +",
+            full_screen: false,
+            resizable: true,
+            width: 800,
+            height: 600
+        })
+        .unwrap_or_else(|error_message| panic!("Error starting engine: {}", error_message));
 
     engine.run();
 }
