2b37ce1500d65853106b426a76393eb61a09f614
Chloe Strainge <nullptr.0@gmail.com>
2019-05-26 20:04:19 -0700

    Removing some non-core features from the core library.

--- lib/vectrex_core/src/lib.rs
+++ lib/vectrex_core/src/lib.rs
@@ -4,8 +4,6 @@ extern crate vectrex_logging;
 extern crate vectrex_engine;
 extern crate vectrex_os_shell;
 extern crate vectrex_gfx;
-extern crate vectrex_gfx_gui;
-extern crate vectrex_srl;
 
 
 
--- lib/vectrex_core/sub_crates/srl/src/lib.rs
+++
@@ -1,6 +0,0 @@
-
-extern crate nom;
-extern crate nom_locate;
-extern crate nom_trace;
-extern crate nom_reader;
-extern crate cranelift;
